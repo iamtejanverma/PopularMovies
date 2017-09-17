de48a8c3d6389e4c12bd7bcffd512069bf1f8a2e
tejanverma <tjayrk@gmail.com>
2017-09-17 15:09:13 +0530

    Adding Menu and Inflating Menu

--- app/src/main/java/com/rk/tjay/popularmovies/DetailActivity.java
+++ app/src/main/java/com/rk/tjay/popularmovies/DetailActivity.java
@@ -1,13 +1,27 @@
 package com.rk.tjay.popularmovies;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class DetailActivity extends AppCompatActivity {
+
+    private Movies currentMovie;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.detail_movie, menu);
+        return true;
+    }
+
+
 }
